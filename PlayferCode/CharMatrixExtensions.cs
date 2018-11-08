7ad88f965508616f70049f866d8aafb154a38ab6
i-ka <i-ka2010@mail.ru>
2018-11-08 23:07:44 +0500

    task

---
+++ PlayferCode/CharMatrixExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayferCode
+{
+    public static class CharMatrixExtensions
+    {
+        public static (int, int) Find(this char[,] matrix, char c)
+        {
+            for (var i = 0; i < matrix.Length; i++)
+            {
+                var row = i / (matrix.GetUpperBound(0) + 1);
+                var col = i % (matrix.GetUpperBound(1) + 1);
+                if (matrix[row, col] == c)
+                    return (row, col);
+            }
+            return (-1, -1);
+        }
+    }
+}
--- PlayferCode/Program.cs
+++ PlayferCode/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MoreLinq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,134 @@ namespace PlayferCode
     {
         static void Main(string[] args)
         {
+            var keyword = "testkeyword";
+            var textToEncode = @"dsoooo";
+
+            var sw = Stopwatch.StartNew();
+            var encoded = Encode(textToEncode.ToLower(), keyword.ToLower());
+            Console.WriteLine(encoded);
+            var decoded = Decode(encoded, keyword.ToLower());
+            Console.WriteLine(decoded);
+            Console.WriteLine($"Done in {sw.ElapsedMilliseconds}ms");
+            Console.ReadKey();
+        }
+
+        private const string Alphabet = "abcdefghijklmnoprstuvwxyz";
+
+        private static string Encode(string textToEncode, string keyword)
+        {
+            var matrix = BuildMatrix(keyword);
+
+            var textWithoutSpaces = textToEncode.Where(c => !char.IsWhiteSpace(c) && !char.IsSeparator(c)).ToList();
+            for (var i = 0; i < textWithoutSpaces.Count; i++)
+            {
+                if (i == textWithoutSpaces.Count - 1 )
+                {
+                    if(textWithoutSpaces.Count % 2 == 1)
+                    {
+                        textWithoutSpaces.Add('x');
+                    }
+                    break;
+                }
+                else if (textWithoutSpaces[i] == textWithoutSpaces[i + 1])
+                {
+                    textWithoutSpaces.Insert(i + 1, 'x');
+                }
+            }
+            var bigramms = textWithoutSpaces.Batch(2).Select(b => string.Join(string.Empty, b)).ToArray();
+
+            for (var i = 0; i < bigramms.Length; i ++)
+            {
+                var first = matrix.Find(bigramms[i][0]);
+                var second = matrix.Find(bigramms[i][1]);
+                if (first.Item1 == second.Item1)
+                {
+                    var firstReplacementPos = (first.Item1, (first.Item2 + 1) % (matrix.GetUpperBound(1) + 1));
+                    var secondReplacementPos = (second.Item1, (second.Item2 + 1) % (matrix.GetUpperBound(1) + 1));
+                    bigramms[i] = new string(new char[]
+                    {
+                        matrix[firstReplacementPos.Item1, firstReplacementPos.Item2],
+                        matrix[secondReplacementPos.Item1, secondReplacementPos.Item2]
+                    });
+                    continue;
+                }
+                if (first.Item2 == second.Item2)
+                {
+                    var firstReplacementPos = ((first.Item1 + 1) % matrix.GetUpperBound(0), first.Item2 );
+                    var secondReplacementPos = ((second.Item1 + 1) % matrix.GetUpperBound(0), second.Item2);
+                    bigramms[i] = new string(new char[]
+                    {
+                        matrix[firstReplacementPos.Item1, firstReplacementPos.Item2],
+                        matrix[secondReplacementPos.Item1, secondReplacementPos.Item2]
+                    });
+                    continue;
+                }
+                bigramms[i] = new string(new char[]
+                {
+                    matrix[first.Item1, second.Item2],
+                    matrix[second.Item1, first.Item2]
+                });
+            }
+
+            return string.Join(string.Empty, bigramms);
+        }
+
+        private static string Decode(string encodedText, string keyword)
+        {
+            var matrix = BuildMatrix(keyword);
+            var bigramms = encodedText.Batch(2).Select(b => string.Join(string.Empty, b)).ToArray();
+
+            for (var i = 0; i < bigramms.Length; i++)
+            {
+                var first = matrix.Find(bigramms[i][0]);
+                var second = matrix.Find(bigramms[i][1]);
+                if (first.Item1 == second.Item1)
+                {
+                    var firstReplacementPos = (first.Item1, (first.Item2 + matrix.GetUpperBound(1) - 1) % matrix.GetUpperBound(1));
+                    var secondReplacementPos = (second.Item1, (second.Item2 + matrix.GetUpperBound(1) - 1) % matrix.GetUpperBound(1));
+                    bigramms[i] = new string(new char[]
+                    {
+                        matrix[firstReplacementPos.Item1, firstReplacementPos.Item2],
+                        matrix[secondReplacementPos.Item1, secondReplacementPos.Item2]
+                    });
+                    continue;
+                }
+                if (first.Item2 == second.Item2)
+                {
+                    var firstReplacementPos = ((first.Item1 + matrix.GetUpperBound(0) - 1) % matrix.GetUpperBound(0), first.Item2);
+                    var secondReplacementPos = ((second.Item1 + matrix.GetUpperBound(0) - 1) % matrix.GetUpperBound(0), second.Item2);
+                    bigramms[i] = new string(new char[]
+                    {
+                        matrix[firstReplacementPos.Item1, firstReplacementPos.Item2],
+                        matrix[secondReplacementPos.Item1, secondReplacementPos.Item2]
+                    });
+                    continue;
+                }
+                bigramms[i] = new string(new char[]
+                {
+                    matrix[first.Item1, second.Item2],
+                    matrix[second.Item1, first.Item2]
+                });
+            }
+
+            return string.Join(string.Empty, bigramms);
+        }
+
+        private static char[,] BuildMatrix(IEnumerable<char> keyword)
+        {
+            var matrix = new char[5, 5];
+            var cleanedKeyword = keyword.Distinct().ToArray();
+            var leftLetters = Alphabet.Where(c => !keyword.Contains(c));
+            for (var i = 0; i < matrix.Length; i++)
+            {
+                if (i < cleanedKeyword.Length)
+                {
+                    matrix[i / 5, i % 5] = cleanedKeyword[i];
+                    continue;
+                }
+                matrix[i / 5, i % 5] = leftLetters.ElementAt(i - cleanedKeyword.Length);
+            }
+            return matrix;
         }
     }
 }
